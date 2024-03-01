806395495bb0ebb877e4b65743abc42b1f6193de
Daniel Garnier-Moiroux <git@garnier.wf>
2024-03-01 15:25:43 +0100

    feat: set authentication entry point to /

--- src/main/java/wf/garnier/webauthn/SecurityConfiguration.java
+++ src/main/java/wf/garnier/webauthn/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.ui.DefaultLogoutPageGeneratingFilter;
 
 @Configuration
@@ -27,6 +28,7 @@ class SecurityConfiguration {
 			.addFilter(new DefaultLogoutPageGeneratingFilter())
 			.csrf(csrf -> csrf.ignoringRequestMatchers("/logout"))
 			.logout(logout -> logout.logoutSuccessUrl("/"))
+			.exceptionHandling(exception -> exception.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")))
 			.build();
 	}
 
