19e98d51294cbbc8b5011a11d72692b55808fc5c
Daniel Garnier-Moiroux <git@garnier.wf>
2024-03-04 13:23:53 +0100

    app: remove unused fields in CredentialsVerification/CredentialsRegistration

--- src/main/java/wf/garnier/webauthn/models/webauthn/CredentialsRegistration.java
+++ src/main/java/wf/garnier/webauthn/models/webauthn/CredentialsRegistration.java
@@ -2,8 +2,6 @@ package wf.garnier.webauthn.models.webauthn;
 
 public record CredentialsRegistration(String id, Response response, String name) {
 
-	// Note: authenticatorData + publicKey unused? They are in the attestationObject
-	public record Response(String attestationObject, String clientDataJSON, String authenticatorData,
-			String publicKey) {
+	public record Response(String attestationObject, String clientDataJSON) {
 	}
 }
--- src/main/java/wf/garnier/webauthn/models/webauthn/CredentialsVerification.java
+++ src/main/java/wf/garnier/webauthn/models/webauthn/CredentialsVerification.java
@@ -1,6 +1,6 @@
 package wf.garnier.webauthn.models.webauthn;
 
-public record CredentialsVerification(String id, String rawId, Response response) {
+public record CredentialsVerification(String id, Response response) {
 	public record Response(String authenticatorData, String clientDataJSON, String signature) {
 	}
 }
